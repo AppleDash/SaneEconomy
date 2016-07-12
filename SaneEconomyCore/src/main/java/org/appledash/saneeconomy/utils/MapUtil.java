3b27b8a1928cc13237a54b33e56a48f49146e1df
AppleDash <derp@derpy.fr>
2016-07-11 20:25:51 -0400

    Make /balancetop sort in the right order.

--- SaneEconomyCore/src/main/java/org/appledash/saneeconomy/utils/MapUtil.java
+++ SaneEconomyCore/src/main/java/org/appledash/saneeconomy/utils/MapUtil.java
@@ -11,7 +11,7 @@ public class MapUtil {
     public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
         List<Map.Entry<K, V>> list =
                 new LinkedList<>(map.entrySet());
-        Collections.sort(list, (o1, o2) -> (o1.getValue()).compareTo(o2.getValue()));
+        Collections.sort(list, (o1, o2) -> -((o1.getValue()).compareTo(o2.getValue())));
 
         Map<K, V> result = new LinkedHashMap<>();
         for (Map.Entry<K, V> entry : list) {
