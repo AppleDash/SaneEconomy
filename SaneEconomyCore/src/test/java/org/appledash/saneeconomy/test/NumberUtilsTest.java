65f625db462767ad54c1071be9e77fd73df137b3
AppleDash <derp@derpy.fr>
2016-08-04 15:18:12 -0400

    Version 0.6.6

--- SaneEconomyCore/src/test/java/org/appledash/saneeconomy/test/NumberUtilsTest.java
+++ SaneEconomyCore/src/test/java/org/appledash/saneeconomy/test/NumberUtilsTest.java
@@ -40,6 +40,8 @@ public class NumberUtilsTest {
         } catch (Throwable e) {
             Locale.setDefault(old);
             throw e;
+        } finally {
+            Locale.setDefault(old);
         }
     }
 }
