c5634ea562f666573abb952417d0f803a3098229
Bayu Aditya H <git-b@yuah.web.id>
2017-11-09 18:24:42 +0700

    Fix building testing issue if system is using comma as decimal separator.

--- SaneEconomyCore/src/test/java/org/appledash/saneeconomy/test/CurrencyTest.java
+++ SaneEconomyCore/src/test/java/org/appledash/saneeconomy/test/CurrencyTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 
 /**
  * Created by AppleDash on 7/29/2016.
@@ -13,8 +14,16 @@ import java.text.DecimalFormat;
 public class CurrencyTest {
     @Test
     public void testCurrencyFormat() {
+        DecimalFormatSymbols testSymbols = new DecimalFormatSymbols();
+        char decimalSeparator=testSymbols.getDecimalSeparator();
+        
         Currency currency = new Currency("test dollar", "test dollars", new DecimalFormat("0.00"));
-        Assert.assertEquals(currency.formatAmount(1.0D), "1.00 test dollar");
-        Assert.assertEquals(currency.formatAmount(1337.0D), "1337.00 test dollars");
+        if(decimalSeparator == ',') { // French, Indonesian.
+            Assert.assertEquals(currency.formatAmount(1.0D), "1,00 test dollar");
+            Assert.assertEquals(currency.formatAmount(1337.0D), "1337,00 test dollars");
+        } else {
+            Assert.assertEquals(currency.formatAmount(1.0D), "1.00 test dollar");
+            Assert.assertEquals(currency.formatAmount(1337.0D), "1337.00 test dollars");
+        }
     }
 }
